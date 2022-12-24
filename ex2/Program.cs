7e1b821a34ce45735eb7ded38cb29bfed5a7cca1
gunel <lothlovien@gmail.com>
2022-12-24 19:29:34 +0400

    решение Задачи №2

---
+++ ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+int min = b;
+
+if (a > b) 
+{  
+    max = a;
+    min = b;
+}
+else
+{
+      max = b;
+      min = a;
+} 
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
